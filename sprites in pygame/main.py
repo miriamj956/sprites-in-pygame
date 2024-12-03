77bfc1e5459ffff842affa1b3db7f71f0718508b
miriamj956 <117miradam@nhusd.k12.ca.us>
2024-12-02 20:23:05 -0800

    Update main.py

--- sprites in pygame/main.py
+++ sprites in pygame/main.py
@@ -9,7 +9,7 @@ screen = pygame.display.set_mode([screen_width, screen_height])
 bkg = pygame.image.load("bkg.jpg")
 bkg = pygame.transform.scale(bkg, (700,500))
 
-#player class
+#player class       libary in libary
 class Player(pygame.sprite.Sprite):
     #attribute
     def __init__(self):
@@ -18,7 +18,24 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.image.load("spaceship.png")
         self.image = pygame.transform.scale(self.image, (70,100))
         self.rect = self.image.get_rect()
-
+    
+    def update(self, pressed_keys):
+        if(pressed_keys[pygame.K_UP]):
+            self.rect.move_ip(0,-5)
+        if (pressed_keys[pygame.K_DOWN]):
+            self.rect.move_ip(0,5)
+        if (pressed_keys[pygame.K_LEFT]):
+            self.rect.move_ip(-5,0)
+        if (pressed_keys[pygame.K_RIGHT]):
+            self.rect.move_ip(5,0)
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > screen_width:
+            self.rect.right = screen_width
+        if self.rect.top < 0:
+            self.rect.top = 0
+        if self.rect.bottom > screen_height:
+            self.rect.bottom = screen_height
 
 #create a group 
 sprites = pygame.sprite.Group()
@@ -33,7 +50,8 @@ def gameStart():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit(0)
-                
+        pressed_keys = pygame.key.get_pressed()
+        rocket.update(pressed_keys)        
         screen.blit(bkg, (0,0))
         sprites.draw(screen)
         pygame.display.update()
